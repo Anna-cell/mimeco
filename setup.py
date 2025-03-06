da6aad6f5688660448206b5590c318476797c666
Anna-cell <annalrt@hotmail.fr>
2025-03-06 11:44:54 +0100

    add ccrossfed metabolites analysis options.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 name='mimeco',
-version='0.1',
+#version='0.1.3',
 author='Anna Lambert',
 author_email='anna.lambert@univ-nantes.fr',
 description='Multi-objective GEMs metabolic interaction inference',
@@ -12,10 +12,10 @@ classifiers=[
 #'License :: OSI Approved :: MIT License',
 'Operating System :: OS Independent',
 ],
-python_requires='>=3.7',
+#python_requires='>=3.7',
 install_requires=[
-'pandas >= 0.22', #should be updated from cobra anyway
-'cobra >= 1.3',
+'pandas >= 1.3', #should be updated from cobra anyway
+'cobra >= 0.22',
 'matplotlib >= 3.5',
 'scikit-learn >= 1.0',
 'matplotlib >= 3.5'])
